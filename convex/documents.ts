368e57be1e2da867cd0b20cc750f496ce88f66b6
aniruddha3919 <aniruddhapal43@gmail.com>
2025-03-31 18:43:41 +0530

    getting data from database

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,18 +1,36 @@
 import { mutation, query } from "./_generated/server";
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 
+// fetching data from table 
 export const getDocuments = query({
-    async handler(ctx){
-        return await ctx.db.query('documents').collect()
+    async handler(ctx) {
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+        console.log(userId);
+        if (!userId) {
+            return [];
+        }
+
+        return await ctx.db.query('documents')
+            .withIndex('by_tokenIdentifier', (q) => q.eq('tokenIdentifier', userId))
+            .collect();
     }
 })
+//creating database table
 export const createDocument = mutation({
     args: {
         title: v.string()
     },
     async handler(ctx, args) {
+        //  verification of authenticated user
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+        console.log(userId);
+        if (!userId) {
+            throw new ConvexError('Not authenticated');
+        }
+        // inserting text in database
         await ctx.db.insert('documents', {
             title: args.title,
+            tokenIdentifier: userId
         })
     },
 })
